d61dd441e1a5846f4fc354c2bf4b46f206474b85
ssafaryanc <ssafaryanc@gmail.com>
2023-02-19 01:30:34 +0300

    Добавил функцию выгрузки списка вебинаров в шедулер

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,8 +1,12 @@
 from rocketry import Rocketry
+from core.logger import logger as log
+from app.integration.bizon365 import services
 
 app = Rocketry(execution="async")
 
 
-@app.task('every 5 seconds')
-async def ping():
-    print("PONG")
+@app.task('every 1 hours')
+async def actual_webinar_rooms():
+    """ Актуализация списка вебинарных комнат из внешнего источника """
+    log.debug(f"Запущен таск актуализации списка вебинарных комнат из внешнего источника")
+    await services.webinars_get_subpages()
