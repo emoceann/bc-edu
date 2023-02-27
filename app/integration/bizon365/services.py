ee60af8477318c9d360a865cd5e3a51e800555ab
ssafaryanc <ssafaryanc@gmail.com>
2023-02-27 20:02:08 +0300

    Добавил функцию вызова отчета по вебинару

--- app/integration/bizon365/services.py
+++ app/integration/bizon365/services.py
@@ -19,4 +19,22 @@ async def webinars_get_subpages():
         log.debug(f"{'Добавлена' if crt else 'Обновлена'} информация о комнате[{room.id}] - \"{room.title}\"")
 
 
+async def webinars_refresh_reports():
+    """ Получение список доступных отчетов и последующее обновление БД"""
+    response: httpx.Response = httpx.get(
+        url=f"{settings.BIZON365_API_HOST}/webinars/reports/getlist",
+        headers={"X-Token": settings.BIZON365_X_TOKEN}
+    )
+    assert response.status_code == 200, "Ошибка сервера Бизон365"
+    print(response.json())
+
+
+async def webinars_get_detail_report(webinar_id: str):
+    """ Получение детального отчета """
+    response: httpx.Response = httpx.get(
+        url=f"{settings.BIZON365_API_HOST}/webinars/reports/get?webinarId={webinar_id}",
+        headers={"X-Token": settings.BIZON365_X_TOKEN}
+    )
+    assert response.status_code == 200, "Ошибка сервера Бизон365"
+    print(response.json())
 
