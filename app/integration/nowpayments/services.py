4b19c04281f4a5f2808b40e7ce7f067b52452bf6
ssafaryanc <ssafaryanc@gmail.com>
2023-03-05 22:23:25 +0300

    Рефактор

--- app/integration/nowpayments/services.py
+++ app/integration/nowpayments/services.py
@@ -7,17 +7,19 @@ from core.logger import logger as log
 
 async def create_payment(user_id: int, invoice_id: int, coin: str):
     async with httpx.AsyncClient() as client:
-        response = await client.post(headers={'x-api-key': settings.NOWPAYMENT_TOKEN,
-                                              'Content-Type': 'application/json',
-                                              },
-                                     url=settings.NOWPAYMENT_URL,
-                                     json={
-                                            "iid": invoice_id,
-                                            "pay_currency": coin,
-                                            "order_description": user_id
-                                            # "ipn_callback_url": settings.NOWPAYMENT_URL не доступен в сэндбоксе, но есть в офиц апи
-                                        })
-        assert response.status_code == 200
+        response = await client.post(
+            headers={
+                'x-api-key': settings.NOWPAYMENT_TOKEN,
+                'Content-Type': 'application/json',
+            },
+            url=settings.NOWPAYMENT_URL,
+            json={
+                "iid": invoice_id,
+                "pay_currency": coin,
+                "order_description": user_id
+                # "ipn_callback_url": settings.NOWPAYMENT_URL не доступен в сэндбоксе, но есть в офиц апи
+            })
+        assert response.status_code == 200, f"Ошибка NowPayments, {response.status_code}"
         wallet = response.json()['pay_address']
         log.debug(f'Юзеру - {user_id} был выдан этот кошелек {wallet}, для оплаты {invoice_id}')
         return wallet
