e73f223496aaa5d7cd7ccd622a418c162cdf6c1f
emoceann <shexaergashev@gmail.com>
2023-03-11 22:23:15 +0500

    аналитика

--- app/integration/bizon365/services.py
+++ app/integration/bizon365/services.py
@@ -3,6 +3,7 @@ from .dao import *
 from .models import *
 from core.settings import settings
 from core.logger import logger as log
+from datetime import datetime
 
 
 async def webinars_get_subpages():
@@ -42,3 +43,38 @@ async def webinars_get_detail_report(webinar_id: str):
 async def create_link_webinar(webinar: str, username: str, email: str, tg_id: str) -> str:
     """ Создание ссылки на вебинар """
     return f"{settings.BIZON365_BC_HOST}/room/{webinar}?email={email}&username={username}&cf_m_tg={tg_id}&autologin"
+
+
+async def webinars_from_today():
+    date = datetime.today()
+    return await WebinarRoom.filter(closest_date__range=(date.min, date.max)).only('original_report')
+
+
+async def count_webinar_users_day():
+    count = 0
+    for i in await webinars_from_today():
+        res = ReportInsideModel.parse_obj(i.original_report['report'])
+        count += len(res.report.rating)
+    return count
+
+
+async def count_webinar_users_by_time(time: int):
+    count = 0
+    for i in await webinars_from_today():
+        users_meta = ReportInsideModel.parse_obj(i.original_report['report']).report.usersMeta.values()
+        for users in users_meta:
+            millis = users['viewTill'] - users['view']
+            hour = (millis / (1000 * 60 * 60)) % 24
+            if hour < time:
+                count += 1
+    return count
+
+
+async def count_webinar_users_ban():
+    count = 0
+    for i in await webinars_from_today():
+        users_meta = ReportInsideModel.parse_obj(i.original_report['report']).report.usersMeta.values()
+        for i in users_meta:
+            if i['ban']:
+                count += count
+    return count
--- app/statistics/services.py
+++ app/statistics/services.py
@@ -5,7 +5,7 @@ from app.account import services as account_service
 class StatisticModelBuilder(StatisticModel):
 
     async def _count_use_bot(self):
-        self.count_use_bot = await account_service.find_count_users()
+        self.bot_users_count = await account_service.find_count_users()
 
     async def build(self) -> StatisticModel:
         await self._count_use_bot()
