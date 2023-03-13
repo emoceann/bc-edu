4815320a542b87a37481b371ebab85d7d287faae
emoceann <shexaergashev@gmail.com>
2023-03-13 23:47:41 +0500

    конец стейтов, исправление миграций, статистика, моделии!!!

--- app/statistics/models.py
+++ app/statistics/models.py
@@ -1,14 +1,17 @@
 class StatisticModel:
-    count_use_bot: int
+    count_user_bot: int
     count_webinar_user: int
+    count_newbie_test_finished: int
+    count_newbie_knowledge_base_red: int
+    count_users_newbie: int
+    count_users_expert: int
+    count_users_test_above_six: int
 
     count_webinar_users_by_1_hour: int
     count_webinar_users_by_2_hour: int
     count_webinar_users_by_3_hour: int
-
     count_webinar_users_ban: int
-    count_newbie_test_finished: int
-    count_newbie_knowledge_base_red: int
-    count_users_newbie_or_expert: int
-    count_users_test_above_six: int
 
+    count_traffic_all: int
+    count_traffic_telegram: int
+    count_traffic_target: int
